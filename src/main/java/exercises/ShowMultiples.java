77b049523121457615b32866ec4308568469336d
Miria Bernardes <miria.bernardes@mercadolivre.com>
2021-06-28 16:12:08 -0300

    implementing algorithm to display multiples of any number

---
+++ src/main/java/exercises/ShowMultiples.java
@@ -0,0 +1,25 @@
+package src.main.java.exercises;
+
+import java.util.Scanner;
+
+public class ShowMultiples {
+
+    public static void main(String[] args) {
+        System.out.println("Digite a quantidade de multiplos: ");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite outro número qualquer: ");
+        Scanner inputTwo = new Scanner(System.in);
+
+        int num = input.nextInt();
+        int num2 = inputTwo.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (num2 % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
